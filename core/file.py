b5fe01262c919315684593ed7c56b4053fed87ee
rdziwinski <rdziwinski@gmail.com>
2017-08-17 21:02:43 +0200

    Add core package with classes written in the past.

---
+++ core/file.py
@@ -0,0 +1,38 @@
+from core.statements import *
+import json
+
+
+class File:
+    data = []
+
+    def __init__(self, file_name='clients.json', mode='r'):
+        self.file_name = file_name
+        self.mode = mode
+        self.read_file()
+
+    def read_file(self):
+        try:
+            with open(self.file_name, self.mode) as (settings_data):
+                self.data = json.load(settings_data)
+                return self.data
+        except Exception as err:
+            error = Statements()
+            sys.exit(error.get_statement(err, "Wlasny komunikat."))
+
+    def get_record(self, level_1, level_2=""):
+        try:
+            if level_1 == "" and level_2 == "":
+                return self.data
+            elif level_1 != "" and level_2 == "":
+                return self.data[level_1]
+            elif level_1 == "" and level_2 != "":
+                record = []
+                for key, value in self.data.items():
+                    record.append(value[level_2])
+                return record
+            else:
+                return self.data[level_1][level_2]
+        except Exception as err:
+            error = Statements()
+            statement = error.get_statement(err, str(err))
+            return statement
---
+++ core/statements.py
@@ -0,0 +1,27 @@
+from core.file import *
+import os
+import sys
+
+
+class Statements:
+
+    def __init__(self):
+        settings = File("manager.json")
+        self.lang = settings.get_record("lang")
+        self.debug = settings.get_record("debug")
+        self.statements_data = File("statements.json").read_file()
+
+    def get_statement(self, err, own=""):
+        if self.debug == 1:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+            statement = "Error in " + file_name + ":" + str(exc_tb.tb_lineno) + ": \n"\
+                        + str(err.__class__.__name__) + ": " + str(err) + "\n" \
+                        + "Error doc: " + err.__doc__ + " " + "\n" + own
+            return statement
+        else:
+            if err.__class__.__name__ in self.statements_data[self.lang]:
+                return self.statements_data[self.lang][err.__class__.__name__] + "\n" + own
+            else:
+                return str(err.__class__.__name__) + ": " + str(err) + "\n" + own
